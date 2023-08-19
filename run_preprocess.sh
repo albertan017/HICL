4943ab317326c46c2bc1c37788a0377f50fe899a
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-19 20:33:35 +0800

    Update run_preprocess.sh

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -1,17 +1,22 @@
+# First we tokenize the #database, --task_name refers to the name of text file
 python preprocess_data_token.py \
---task_name hash_database \
---tokenizer_name albertan017/hashencoder
+  --task_name hash_database \
+  --tokenizer_name albertan017/hashencoder
 
+# Then we leverage #Encoder to extract features for the tweets
 python preprocess_extract_feature.py \
---dataset_path hash_database \
---model_name albertan017/hashencoder \
---input_dir ../finetune/data/ \
---save './hash_database_feature/
+  --dataset_path hash_database \
+  --model_name albertan017/hashencoder \
+  --save './hash_database_feature/
 
+# Finally we search for the most topic similar tweets from #database
+# --hash_file is the extracted feature embeddings from previous step
+# --dataset_path is the downstream benchmarks path
+# --task_name is the downstream tasks
+# --method is the suffix to save the search results
 python preprocess_match.py \
---hash_file ./hash_database_feature/hash_database \
---model_name albertan017/hashencoder \
---dataset_path ../finetune/data/ \
---task_name eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm \
---method _hicl
-
+  --hash_file ./hash_database_feature/hash_database \
+  --model_name albertan017/hashencoder \
+  --dataset_path ../finetune/data/ \
+  --task_name eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm \
+  --method _hicl
