dd0bd0ddf7d96f25d72ae1d3c24272aec3318ad4
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-19 19:49:09 +0800

    Update preprocess_data_token.py

--- HICL/preprocess_data_token.py
+++ HICL/preprocess_data_token.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument("--output_dir", default='./', type=str, required=False, help="The output directory where the model predictions and checkpoints will be written.")
 parser.add_argument("--dataset_path", default='./', type=str, required=False, help="dataset name")
-parser.add_argument("--task_name", default='tweet_hash_clean', type=str, required=False, help="dataset name")
+parser.add_argument("--task_name", default='hash_database', type=str, required=False, help="dataset name")
 parser.add_argument("--use_slow_tokenizer", action="store_true", help="If passed, will use a slow tokenizer (not backed by the 🤗 Tokenizers library).")
 parser.add_argument("--tokenizer_name", default='vinai/bertweet-base', type=str, required=False, help="tokenizer name")
 parser.add_argument("--max_seq_length", default=128, type=int, help="The maximum total input sequence length after tokenization. Sequences longer than this will be truncated, sequences shorter will be padded.")
