e7a4971b13b61716b751c5927be407d1a6ebc1be
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-19 21:49:40 +0800

    Update run_preprocess.sh

--- run_preprocess.sh
+++ run_preprocess.sh
@@ -17,6 +17,8 @@ python preprocess_extract_feature.py \
 python preprocess_match.py \
   --hash_file ./hash_database_feature/hash_database \
   --model_name albertan017/hashencoder \
-  --dataset_path ../finetune/data/ \
+  --dataset_path ../data/ \
   --task_name eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm \
   --method _hicl
+
+# The bash will create {SPLIT}_hicl_top100_sp.json file at the same folder of input data
