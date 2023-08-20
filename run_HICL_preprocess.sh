59ca7929bc0439fcb51a123898b68b560f3d4d60
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-20 16:47:50 +0800

    Update run_HICL_preprocess.sh

--- run_HICL_preprocess.sh
+++ run_HICL_preprocess.sh
@@ -1,13 +1,15 @@
 # First we tokenize the #database, --task_name refers to the name of text file
 python preprocess_data_token.py \
+  --output_dir ./data/
+  --dataset_path ./data/
   --task_name hash_database \
   --tokenizer_name albertan017/hashencoder
 
 # Then we leverage #Encoder to extract features for the tweets
 python preprocess_extract_feature.py \
-  --dataset_path hash_database \
+  --dataset_path ./data/hash_database \
   --model_name albertan017/hashencoder \
-  --save ./hash_database_feature/
+  --save ./data/hash_database_feature/hash_database
 
 # Finally we search for the most topic similar tweets from #database
 # --hash_file is the extracted feature embeddings from previous step
@@ -15,9 +17,9 @@ python preprocess_extract_feature.py \
 # --task_name is the downstream tasks
 # --method is the suffix to save the search results
 python preprocess_match.py \
-  --hash_file ./hash_database_feature/hash_database \
+  --hash_file ./data/hash_database_feature/hash_database \
   --model_name albertan017/hashencoder \
-  --dataset_path ../data/ \
+  --dataset_path ./data/ \
   --task_name eval-stance,eval-emotion,eval-irony,eval-offensive,eval-hate,sem21-task7-humor,sem22-task6-sarcasm \
   --method _hicl
 
