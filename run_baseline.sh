8e2989daee80bdbb6e4d33979b35e6a0c7dab2b3
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-20 16:49:28 +0800

    Update run_baseline.sh

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,3 +1,3 @@
 # Please refer to the argparse definition for parameter details
-python finetune_data_token.py
-python finetune.py
+python finetune_data_token.py --output_dir ./data/ --dataset_path ./data/
+python finetune.py --input_dir ./data/
