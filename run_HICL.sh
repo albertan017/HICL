92c0b7b049b35f46b22d1724c57b2f4f5994584f
albertan017 <142430876+albertan017@users.noreply.github.com>
2023-08-20 16:50:14 +0800

    Update run_HICL.sh

--- run_HICL.sh
+++ run_HICL.sh
@@ -1,3 +1,3 @@
 # Please refer to the argparse definition for parameter details
-python HICL_data_token.py
-python HICL.py
+python HICL_data_token.py  --output_dir ./data/ --dataset_path ./data/
+python HICL.py --input_dir ./data/
